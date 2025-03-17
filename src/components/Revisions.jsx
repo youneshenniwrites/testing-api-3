c07e96cc695cd5e3b5287ee7f31c2e9d48e6581a
youneshenniwrites <youneshenniwrites@gmail.com>
2025-03-17 17:45:28 +0000

    hooks

--- src/components/Revisions.jsx
+++ src/components/Revisions.jsx
@@ -1,38 +1,9 @@
 import React, { useEffect, useState } from "react";
 
 export default function Revisions() {
-  const [users, setUsers] = useState(null);
   const [query, setQuery] = useState("");
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState();
-
-  const [debouncedValue, setDebouncedValue] = useState("");
-
-  useEffect(() => {
-    try {
-      const fetchUsers = async () => {
-        const response = await fetch("https://api.whatever.com/");
-        if (!response.ok) throw Error("Failed to fetch users");
-        const data = await response.json();
-        setUsers(data.results);
-      };
-
-      fetchUsers();
-    } catch (err) {
-      console.log(err);
-      setError(err);
-    } finally {
-      setLoading(false);
-    }
-  }, []);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setDebouncedValue(query);
-    }, 300);
-
-    return () => clearTimeout(timer);
-  }, [query]);
+  const debouncedValue = useDebounce(query, 300);
+  const { users, loading, error } = useFetchUsers();
 
   const filteredUsers = users.filter((user) =>
     user.toLowerCase().includes(debouncedValue.toLowerCase())
@@ -55,3 +26,45 @@ export default function Revisions() {
     </div>
   );
 }
+
+// hook 1
+const useFetchUsers = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    try {
+      const fetchUsers = async () => {
+        const response = await fetch("https://api.whatever.com/");
+        if (!response.ok) throw new Error("Failed to fetch users");
+        const data = await response.json();
+        setUsers(data.results);
+      };
+
+      fetchUsers();
+    } catch (err) {
+      console.log(err);
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  return { users, loading, error };
+};
+
+// hook 2
+const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => clearTimeout(timer);
+  }, [value, delay]);
+
+  return debouncedValue;
+};
