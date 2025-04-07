c57a057cb1523a24ff8e3d368724c4c8e549cb0c
youneshenniwrites <youneshenniwrites@gmail.com>
2025-04-07 19:46:41 +0100

    delete

--- src/utils/ computeHighestProbabilityCountry.js
+++
@@ -1,7 +0,0 @@
-export const computeHighestProbabilityCountry = (countries) => {
-  if (!countries || countries.length === 0) return null;
-
-  return countries.reduce((max, item) =>
-    item.probability > max.probability ? item : max
-  );
-};
