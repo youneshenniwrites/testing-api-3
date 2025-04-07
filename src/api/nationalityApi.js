e145ebe643cf76d8a53a1cdce2af5898c582a9ee
youneshenniwrites <youneshenniwrites@gmail.com>
2025-04-07 18:56:57 +0100

    add env

--- src/api/nationalityApi.js
+++ src/api/nationalityApi.js
@@ -1,7 +1,9 @@
+import { NATIONALIZE_API_BASE_URL as BASE_URL } from "../constants/urls";
+
 export const fetchNationality = async (name) => {
   if (!name) return null;
 
-  const response = await fetch(`https://api.nationalize.io/?name=${name}`);
+  const response = await fetch(`${BASE_URL}/?name=${name}`);
   if (!response.ok) {
     throw new Error("Failed to fetch nationality data");
   }
---
+++ src/constants/urls.js
@@ -0,0 +1,2 @@
+export const NATIONALIZE_API_BASE_URL = import.meta.env
+  .VITE_NATIONALIZE_API_URL;
